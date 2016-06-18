d67ae2aee918215b518386309346fceb51265848
Rori Kurniadi <rori.kurniadi@yahoo.com>
2016-06-18 11:09:10 +0700

    user seeder

---
+++ seeders/20160617225833-user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var bcrypt = require('bcrypt');
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      name: 'Admin ROC',
+      email: 'admin@roc.com',
+      password: this.generateHash('password'),
+      remember_token: null,
+      avatar: null,
+      created_at: new Date(),
+      updated_at: new Date()
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+
+  },
+
+  generateHash: function(password) {
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+    return hash;
+  }
+};
